162d2515b355562784a9dbdf77aae565afed4ccf
“zfyildiz” <feyzayildiz.dev@gmail.com>
2024-03-14 18:22:48 +0300

    First commit

---
+++ Day1/FirstDayPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+
+/* variables
+ Variables: allow us to store temporary information in our program,
+ and form a key part of almost every Swift program. */
+
+var greeting = "Hello, playground"
+
+/*Once you create a variable using var, you can change it
+ as often as you want without using var again. For example:*/
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+/* Strings & Integers
+ Swift is what’s known as a type-safe language, which means that every variable must
+ be of one specific type. The str variable that Xcode created for us holds a string of
+ letters that spell “Hello, playground”,so Swift assigns it the type String.*/
+
+var stringValue = "Hello, playground"
+
+//On the other hand, if we want to store someone’s age we might make a variable like this:
+var age = 38
+//That holds a whole number, so Swift assigns the type Int – short for “integer”.
+
+var population = 8_000_000 // this don’t change the number but do make it easier to read
+
+
